7b719a458e9a986f70f4b60ea38911e93cc3dd1c
Brett Duncavage <brett.duncavage@rd.io>
2012-02-11 14:01:25 -0800

    Envrionment WIP

---
+++ static/environment.js
@@ -0,0 +1,10 @@
+function Environment() {
+}
+
+Environment.prototype.start = function(seedMeasures, fitnessData) {
+  // choose random segements
+  var fitnessSegments = [];
+  var randStart =
+  for(var i = 0; i < seedMeasures.length; i++) {
+    
+}
