d9e1c10de526bfbadc8111479763ad9d0423ba39
Jonathon Klobucar <jonathon.klobucar@rd.io>
2012-02-11 16:49:39 -0800

    echonest api class

---
+++ echonest.py
@@ -0,0 +1,56 @@
+from constants import Constants
+import httplib2
+from urllib import urlencode
+import copy
+import json
+
+class ECHONEST_APIS:
+  SONG_SEARCH = 'song/search'
+  SONG_PROFILE = 'song/profile'
+
+
+class Echonest(object):
+  def init(self):
+    # Default EchoNEST URL, can be overridden
+    en_url = Constants.ECHONEST_URL
+    if en_url:
+      self.ECHONEST_URL = en_url
+    else:
+      self.ECHONEST_URL = "http://developer.echonest.com/api/v4/"
+
+  def song_lookup(self, artist, track):
+    keywords = {
+      'format': 'json',
+      'results': 1,
+      'artist': artist,
+      'title': track,
+      }
+    song_response = self._look_up(ECHONEST_APIS.SONG_SEARCH, **keywords)
+
+    song_info = song_response.get('response', {}).get('songs', {})
+    return song_info
+
+  def audio_profile(self, id):
+    keywords = {
+          'id': id,
+          'bucket': 'audio_summary'
+          }
+    audio_profile = self._look_up(ECHONEST_APIS.SONG_PROFILE, **keywords)
+    song_info = audio_profile.get('response', {}).get('songs', {})
+
+    return song_info
+  def _look_up(self, api, **kwargs):
+    args = copy.copy(kwargs)
+    args.update({'api_key': Constants.ECHONEST_KEY})
+    look_up_url = "http://developer.echonest.com/api/v4/"
+    look_up_url += api + '?'
+    encoded_args = urlencode(args)
+    look_up_url += encoded_args
+    http = httplib2.Http()
+    response, content = http.request(look_up_url)
+    json_content = json.loads(content)
+    return json_content
+
+# example http://developer.echonest.com/api/v4/song/profile?api_key=N6E4NIOVYMTHNDM8J&id=SONWCPE12B3A138A4E&bucket=audio_summary
+
+#http://developer.echonest.com/api/v4/song/search?api_key=N6E4NIOVYMTHNDM8J&format=json&results=1&artist=radiohead&title=karma%20police
